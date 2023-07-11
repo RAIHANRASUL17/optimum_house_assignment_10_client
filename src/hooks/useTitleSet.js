07a36dfe56ef71861dbdee32c70daf08a67b670e
RAIHANRASUL17 <smrihanrasul17cqut@gmail.com>
2023-07-11 16:41:42 +0800

    optimum house client has been moved

--- src/hooks/useTitleSet.js
+++ src/hooks/useTitleSet.js
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 
 const useTitleSet = (titleSet) =>{
     useEffect(()=>{
-        // // step-1
+        // // step-1a
         // document.title= titleSet;
         // step-2 dynamic
         document.title= `${titleSet}-FoodRecipes`;
